30bb0897c31cddc964cd452ccbfdb074d5341736
anshika494 <anshi040904@gmail.com>
2025-07-02 12:55:31 +0530

    Made changes

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Button } from "@/components/ui/button";
+
+import { Loader2, Plus } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
+import { DataTable } from "./data-table";
+
+import { 
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+import { columns } from "./columns";
+
+
+const AccountsPage = () => {
+    const newAccount = useNewAccount();
+    const accountsQuery = useGetAccounts();
+    const accounts = accountsQuery.data || [];
+
+    if (accountsQuery.isLoading) {
+        return (
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader>
+                        <Skeleton className="h-8 w-48" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex iems-center justify-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin" />
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
+
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg: justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Accounts page
+                    </CardTitle>
+                    <Button onClick={newAccount.onOpen} size="sm">
+                        <Plus className="size-4 mr-2" />
+                        Add new
+                    </Button>
+                </CardHeader>
+                <CardContent>
+                    <DataTable
+                        filterKey="email"
+                        columns={columns}
+                        data={accounts}
+                        onDelete={() => {}}
+                        disabled={false}
+                    />
+                </CardContent>
+            </Card>
+        </div>
+    );
+};
+
+export default AccountsPage;
--- features/accounts/api/use-get-accounts.ts
+++ features/accounts/api/use-get-accounts.ts
@@ -1,5 +1,6 @@
 
 import { useQuery } from "@tanstack/react-query";
+
 import { client }  from "@/lib/hono";
 
 export const useGetAccounts = () => {
