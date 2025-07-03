954a13cae3986ed5a8f08d961f17c88507d56e39
anshika494 <anshi040904@gmail.com>
2025-07-03 11:59:32 +0530

    feat: add useEditAccount hook for editing account details with success and error notifications

---
+++ features/accounts/api/use-edit-account.ts
@@ -0,0 +1,33 @@
+import { toast } from "sonner";
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<typeof client.api.accounts[":id"]["$patch"]>;
+type RequestType = InferRequestType<typeof client.api.accounts[":id"]["$patch"]>["json"];
+
+export const useEditAccount = (id?: string) => {
+    const queryClient = useQueryClient();
+
+    return useMutation<ResponseType, Error, RequestType>({
+        mutationFn: async (json) => {
+            const response = await client.api.accounts[":id"]["$patch"]({
+                param: { id },
+                json,
+            });
+            return await response.json();
+        },
+        onSuccess: (data) => {
+            toast.success("Account updated");
+
+            queryClient.setQueryData(["account", id], data);
+
+            queryClient.invalidateQueries({ queryKey: ["account", id] });
+            queryClient.invalidateQueries({ queryKey: ["accounts"] });
+        },
+        onError: () => {
+            toast.error("Failed to edit account");
+        },
+    });
+};
