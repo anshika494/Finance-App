464638681f03292826d856368dae27e1ac562f58
anshika494 <anshi040904@gmail.com>
2025-07-04 23:40:47 +0530

    fix: updated select.tsx

---
+++ components/select.tsx
@@ -0,0 +1,55 @@
+import { useMemo } from "react";
+import CreatableSelect from "react-select/creatable";
+import { SingleValue } from "react-select";
+
+type Props = {
+    onChange: (value?: string) => void,
+    onCreate?: (value: string) => void,
+    options?: { label: string; value: string }[];
+    value?: string | null | undefined;
+    disabled?: boolean;
+    placeholder?: string;
+};
+
+export const Select = ({
+    value,
+    onChange,
+    disabled,
+    onCreate,
+    options = [],
+    placeholder
+}: Props) => {
+    const onSelect = (
+        option: SingleValue<{ label: string, value: string }>
+    ) => {
+        onChange?.(option?.value);
+    };
+
+    const formattedValue = useMemo(() => {
+        return options.find((option) => option.value === value);
+    }, [options, value]);
+
+    return (
+        <CreatableSelect
+            placeholder={placeholder}
+            className="text-sm h-10"
+            styles={{
+                control: (base) => ({
+                    ...base,
+                    borderColor: "#e2e8f0",
+                    ":hover": {
+                        borderColor: "#e2e8f0"
+                    },
+                }),
+            }}
+            isDisabled={disabled}
+            options={options}
+            value={formattedValue}
+            onChange={onSelect}
+            onCreateOption={(inputValue) => {
+                onCreate?.(inputValue);
+                onChange?.(inputValue);
+            }}
+        />
+    );
+};
