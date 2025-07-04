cc7bc4ef3c01c9e318f54506a3e109478f7b6833
anshika494 <anshi040904@gmail.com>
2025-07-04 14:36:10 +0530

    feat: implement category management features with create, edit, and delete functionality

---
+++ features/categories/api/use-edit-category.ts
@@ -0,0 +1,33 @@
+import { toast } from "sonner";
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<typeof client.api.categories[":id"]["$patch"]>;
+type RequestType = InferRequestType<typeof client.api.categories[":id"]["$patch"]>["json"];
+
+export const useEditCategory = (id?: string) => {
+    const queryClient = useQueryClient();
+
+    return useMutation<ResponseType, Error, RequestType>({
+        mutationFn: async (json) => {
+            const response = await client.api.categories[":id"]["$patch"]({
+                param: { id },
+                json,
+            });
+            return await response.json();
+        },
+        onSuccess: (data) => {
+            toast.success("Category updated");
+
+            queryClient.setQueryData(["category", id], data);
+
+            queryClient.invalidateQueries({ queryKey: ["category", id] });
+            queryClient.invalidateQueries({ queryKey: ["categories"] });
+        },
+        onError: () => {
+            toast.error("Failed to edit category");
+        },
+    });
+};
